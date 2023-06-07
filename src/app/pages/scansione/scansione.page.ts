62fb3a35feb719387f19be9550cb98abdff34227
leozap00 <leonardo.zappacosta@student.univaq.it>
2023-06-07 12:49:19 +0200

    inizio del barcode scanner

--- src/app/pages/scansione/scansione.page.ts
+++ src/app/pages/scansione/scansione.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {BarcodeScanner} from "@awesome-cordova-plugins/barcode-scanner/ngx";
 
 @Component({
   selector: 'app-scansione',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScansionePage implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private barcodeScanner: BarcodeScanner
+  ) { }
   ngOnInit() {
+
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+    }).catch(err => {
+      console.log('Error', err);
+    });
   }
 
 }
